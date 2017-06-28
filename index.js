bc265e0d8e2fe7f7b937ba308260607dbac9b000
Christopher Brown <nudgechris@gmail.com>
2017-06-28 16:44:04 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html')
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
